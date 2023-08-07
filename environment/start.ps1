08f0c31f7ddbbf8d6c6623a099c3eabcb96eb879
Attila Kapostyak <attilak@gmail.com>
2023-08-07 12:41:23 +0300

    Update Downloads folder

--- environment/start.ps1
+++ environment/start.ps1
@@ -7,7 +7,11 @@ $xml = [xml](Get-Content $filePath)
 # Find the first HostFolder element and update its value
 $firstHostFolder = $xml.SelectSingleNode("//HostFolder[1]")
 $firstHostFolder.InnerText = $newHostFolderValue
-$firstHostFolder
+
+# Find the second HostFolder element and update its value
+$secondHostFolder = $xml.SelectSingleNode("/Configuration/MappedFolders/MappedFolder[2]/HostFolder[1]")
+$secondHostFolder.InnerText = $(Get-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders").PSObject.Properties["{374DE290-123F-4565-9164-39C4925E467B}"].Value 
+
 # Save the updated XML back to the file
 $xml.Save($filePath)
 
